546a005fe24acdc976cc15008b2bf236f35c6987
Emir Azaiez <emir.azaiezsahli@gmail.com>
2024-05-15 13:11:21 +0100

    Add test for module pass

--- ios/Uaepass.mm
+++ ios/Uaepass.mm
@@ -2,6 +2,11 @@
 
 @interface RCT_EXTERN_MODULE(UAEPass, NSObject)
 
+RCT_EXTERN_METHOD(test: (RCTPromiseResolveBlock)resolve rejecter:(RCTPromiseRejectBlock)reject)
+RCT_EXTERN_METHOD(test2: (RCTPromiseResolveBlock)resolve rejecter:(RCTPromiseRejectBlock)reject)
+RCT_EXTERN_METHOD(test3: (RCTPromiseResolveBlock)resolve rejecter:(RCTPromiseRejectBlock)reject)
+
+
 RCT_EXTERN_METHOD(login: (NSDictionary *)params resolve:(RCTPromiseResolveBlock)resolve rejecter:(RCTPromiseRejectBlock)reject)
 RCT_EXTERN_METHOD(logout: (RCTPromiseResolveBlock)resolve rejecter:(RCTPromiseRejectBlock)reject)
 
